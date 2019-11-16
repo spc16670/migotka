c532f521d4e881d37b7dc0edeef2ea1e17f1e0ea
Szymon Czaja <simon@ionas.co.uk>
2019-11-16 19:19:39 +0000

    start work on histograms

--- migotki/plotki/bar/patient.py
+++ migotki/plotki/bar/patient.py
@@ -8,23 +8,25 @@ def plot_average_tpr_for_each_patient():
     data = []
     for p in PATIENTS:
         trials = p.data['BCIFES_Trials']
-        avg = 0
+        sum = 0
         count = len(trials)
         for t in trials:
             tpr = p.get_tpr(t)
             if not np.isnan(tpr):
-                avg += tpr
+                sum += tpr
             else:
                 count -= 1
-        pctg = avg / count * 100
+        pctg = sum / count * 100
         data.append((count, pctg))
 
-    x, y = zip(*data)
-    print(data)
-    plt.xticks(range(len(x)), x)
-    plt.bar(x, y, color='green')
-    #ax.set_xticklabels([str(l) for l in x])
-    #ax.set_xlabel("Number of Sessions")
-    plt.ylabel("Average True Positive Rate (%)")
+    counts, y = zip(*data)
+
+    fig, ax = plt.subplots()
+    x = range(len(counts))
+    ax.bar(x, y, color='green', align="center", alpha=.5)
+    ax.set_xticks(x)
+    ax.set_xticklabels(counts)
+    ax.set_xlabel("Number of Sessions")
+    ax.set_ylabel("Average True Positive Rate (%)")
     plt.title("Average TPR for  Each patient")
     plt.show()
